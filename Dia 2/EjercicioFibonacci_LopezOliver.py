067bafc0d1547d3b3472b984e394eb02fcceb77e
Oli <oliverlopez42069@gmail.com>
2024-01-26 14:03:29 -0500

    Changes

--- Dia 2/EjercicioFibonacci_LopezOliver.py
+++ Dia 2/EjercicioFibonacci_LopezOliver.py
@@ -14,17 +14,41 @@ print("El programa va a repetirse hasta que el usuario ingrese 0 como valor")
 print("En este programa podrás elegir hasta que término la secuencia va a continuar")
 print()
 
-def fibo(n): # def, nombre (parámetros)
+def fibo(n): # def, nombre (variable n (parámetro))
     secuencia = [0, 1] # se crea la variable secuencia la cual cuenta con los valores iniciales (0, 1)
-    while len(secuencia) < n: # while, lenght(variable "secuencia") sea menor a n
+    while len(secuencia) < n: # while, (variable "secuencia") sea menor a n
         secuencia.append(secuencia[-1] + secuencia[-2]) # variable "secuencia".adjuntar(variable secuencia[-1] + variable secuencia[-2]) (-1 y -2 para ubicar los valores correctamente)
     return secuencia # return variable "secuencia"
 
-n = int(input("Ingrese el valor limitante: ")) # variable "n" se le es asignado un valor a ingresar por el usuario como entero
-while n != 0: # while variable "n" no sea igual a cero
-    secuencia = fibo(n) # variable "secuencia" ahora cambia sus variables de acuerdo a la función fibo
-    print(secuencia) # mostrar en pantalla la secuencia resultante
-    n = int(input("Ingrese un nuevo limitante para continuar o cero para terminar: ")) # variable "n" se le es asignado un nuevo valor a ingresar por el usuario como entero o se le da la opción de ingresar cero y terminar el programa
+while True: # while, variable "True"
+    try:
+        n = int(input("Ingrese el valor limitante: ")) # variable "n" se le es asignado el valor ingresado por el usuario
+        print()
+        if n < 0: # if variable "n" es menor a cero entonces
+            print("Error: El valor limitante debe ser positivo") # mostrar mensaje de error
+            print()
+        else:
+            break
+    except ValueError:
+        print("Error: El valor limitante debe ser un número entero")
+        print()
+
+while n != 0:
+    secuencia = fibo(n)
+    print(secuencia)
+    print()
+    while True:
+        try:
+            n = int(input("Ingrese un nuevo valor limitante o ingrese 0 para finalizar el programa: "))
+            print()
+            if n < 0:
+                print("Error: El valor limitante debe ser positivo")
+                print()
+            else:
+                break
+        except ValueError:
+            print("Error: El valor limitante debe ser un número entero")
+            print()
 
 print("El programa ha terminado") # al ingresar cero soltará el anterior mensaje
 
