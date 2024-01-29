a9497c3fa5166ae41da532b18e6e185a94cbe112
oliverlopez42069@gmail.com <a>
2024-01-29 14:00:04 -0500

    MoneyChange update

--- Dia 3/EjercicioMoneyChange_LopezOliver.py
+++ Dia 3/EjercicioMoneyChange_LopezOliver.py
@@ -4,12 +4,28 @@
 
 # ---- Money Change ----
 
-def min_coins(money):
-    dp = [0] + [float('inf')] * money
-    for i in range(1, money + 1):
-        dp[i] = min(dp[i - 1], dp[i - 5] + (i - 5) // 5, dp[i - 10] + (i - 10) // 10)
-    return dp[money] if dp[money] != float('inf') else -1
+def min_coins(money, coins):
+    # Initialize an array to store the minimum number of coins needed for each value up to money
+    coins_needed = [float('inf')] * (money + 1)
+    coins_needed[0] = 0
     
+    # Fill the coins_needed array using a loop
+    for i in range(1, money + 1):
+        # For each coin denomination
+        for coin in coins:
+            # If the coin denomination is less than or equal to the current value
+            if coin <= i:
+                # Update the minimum number of coins needed for the current value
+                coins_needed[i] = min(coins_needed[i], coins_needed[i - coin] + 1)
+    
+    # Return the minimum number of coins needed to change money
+    return coins_needed[money] if coins_needed[money] != float('inf') else -1
 
+# Get the amount from the user
+amount = int(input(""))
+
+# Test the function with the user-provided amount
+coins = [1, 5, 10]
+print(min_coins(amount, coins))
 
 ## Desarrollado por OLIVER EDUARDO LOPEZ PEREZ - 1097911715
