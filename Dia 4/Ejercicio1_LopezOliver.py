00e2c5d319e95dc7921ccbd78a2b672598d77850
Oli <oliverlopez42069@gmail.com>
2024-01-30 15:44:30 -0500

    asdf

--- Dia 4/Ejercicio1_LopezOliver.py
+++ Dia 4/Ejercicio1_LopezOliver.py
@@ -1,11 +1,25 @@
 t = int(input())
 
-nyk 
+x = (input())
+nyk = [*x]
+
+for i in range(2):
+    num=int(nyk[i])
+    nyk[i]=num
+
+y = (input())
+l = [*y]
+
+for i2 in range(nyk[0]):
+    num2=int(l[i2])
+    l[i2]=num2
+
+def info (t):
+    
 
 
-
-for i in range(1,t+1,1):
-    print("Caso #",i,":")
+for c in range(1,t+1,1):
+    print("Caso #",c,":")
 
     
 
