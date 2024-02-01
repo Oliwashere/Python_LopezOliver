1f91984172ace924e16d1ac083960f0701aead96
Oli <oliverlopez42069@gmail.com>
2024-02-01 18:25:10 -0500

    Datastructures

---
+++ Dia 6/EjercicioDataStructures_LopezOliver.py
@@ -0,0 +1,25 @@
+## --------------------------------------------
+## ------- EJERCICIO DATA STRUCTURES ----------
+## --------------------------------------------
+
+# ---- Data structures ----
+
+## Mutable
+
+# Lists
+# Colección ordenada y modificable
+
+## Inmutable
+
+# Strings
+# Seuencia inmutable de caracteres
+
+# Tuples
+# Colección ordenada e inmutable
+
+## Partially mutable
+
+# Dictionaries
+# Estructura de datos clave-valor
+
+## Desarrollado por OLIVER EDUARDO LOPEZ PEREZ - 1097911715
