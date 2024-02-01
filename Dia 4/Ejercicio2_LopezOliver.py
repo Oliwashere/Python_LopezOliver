66aa6d2325433d52a1311b599cd67a14bfb4d32b
Oli <oliverlopez42069@gmail.com>
2024-02-01 07:40:10 -0500

    aaa

---
+++ Dia 4/Ejercicio2_LopezOliver.py
@@ -0,0 +1,11 @@
+## -------------------------------
+## ------- EJERCICIO 2 -----------
+## -------------------------------
+
+# ---- Tipos de moneda ----
+
+
+
+
+
+   ## Desarrollado por OLIVER EDUARDO LOPEZ PEREZ - 1097911715
