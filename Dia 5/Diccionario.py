e573f54672553630b2a1873e8ff7f065377f9841
Oli <oliverlopez42069@gmail.com>
2024-02-01 08:04:04 -0500

    Dicc update

--- Dia 5/Diccionario.py
+++ Dia 5/Diccionario.py
@@ -18,5 +18,21 @@ for i in Diccionario:
         print(Diccionario[valor])
 
 #Imprimir las llaves y valores de un diccionario
-for llavo , valor in Diccionario.items():
-            
+for llave , valor in Diccionario.items():
+            print(llave,valor)
+
+#Limpiar un diccionario
+#Diccionario.clear() #ESTO LIMPIA EL DICCIONARIOOOOOOO
+print(Diccionario)
+
+#Guardar las llaves de un diccionario en una lista
+listallaves=Diccionario.keys()
+print(listallaves)
+
+#Guardar los valores de un diccionario en una lista
+listavalores=Diccionario.values()
+print(listavalores)
+
+#Eliminar una llave de un diccionario
+Diccionario.pop("Nombre")
+print(Diccionario)
